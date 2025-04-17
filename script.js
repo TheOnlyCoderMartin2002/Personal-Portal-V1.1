3abb658b118cedad1d91cf01ba70b64942aecca7
--TheOnly-- <129699910+TheOnlyCoderMartin2002@users.noreply.github.com>
2025-04-17 04:03:22 +0100

    script.js

--- script.js
+++ script.js
@@ -46,14 +46,4 @@ function openCalcJot() {
     WELCOME_PANEL.style.height = "580px";
     MAIN_OPTION_MENU.style.display = "none";
 
-    let closeCalcJot = document.createElement("button");
-    closeCalcJot.innerHTML = "Close Program";
-    closeCalcJot.setAttribute("id", "close-calc-jot");
-    closeCalcJot.setAttribute("class", "num-btn");
-    document.getElementById("root-container").appendChild(closeCalcJot);
-
-    closeCalcJot.onclick = {
-        document.getElementById("calc-jot-app").style.display = "none";
-        closeCalcJot.style.display = "none";
-    }
 }
