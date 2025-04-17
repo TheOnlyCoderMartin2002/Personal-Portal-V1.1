9ff800224cd50a1c295288e991ca2d8681237c6b
--TheOnly-- <129699910+TheOnlyCoderMartin2002@users.noreply.github.com>
2025-04-17 18:11:03 +0100

    script.js

--- script.js
+++ script.js
@@ -34,12 +34,10 @@ function enterBtn() {
 }
 
 function logoutPortal() {
-    WELCOME_PANEL.style.display = "none";
     MAIN_OPTION_MENU.style.display = "none";
     PIN_ACCESS_INP.style.display = "block";
     PIN_ACCESS_PANEL.style.display = "block";
     NUM_KEY_PANEL.style.display = "flex";
-    INFO_PAR.style.display = "flex";
     PIN_CONTROL_PANEL.style.display = "block";
     document.getElementById("logout-portal").style.display = "none";
     document.getElementById("logout-portal").style.margin = "auto";
